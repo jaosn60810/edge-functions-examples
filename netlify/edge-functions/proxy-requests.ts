0a7f8ddb37d3f0876d261815c32eb514678ee4c3
jaosn60810 <jason60810@gmail.com>
2023-01-05 21:45:11 +0800

    chore: change url

--- netlify/edge-functions/proxy-requests.ts
+++ netlify/edge-functions/proxy-requests.ts
@@ -2,7 +2,7 @@ import { Context } from "https://edge.netlify.com";
 
 export default async (request: Request, context: Context) => {
 
-  const joke = await fetch("https://icanhazdadjoke.com/", {
+  const joke = await fetch("https://memes.tw/wtf/api", {
     "headers": {
       "Accept": "application/json"
     }
